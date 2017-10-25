e7735e5d495c9f8590f001c2f7e01019777b1d6c
Khursheed Ali <khursheedali38@gmail.com>
2017-10-25 16:36:07 +0530

    initial build for skull stripping

--- brain_extract.m
+++ brain_extract.m
@@ -1,16 +1,17 @@
 %read  image
-I = imread('brain_skull.jpg') ;
+I = imread('brain_up.jpg') ;
 
 %convert 2 gray scale
 I = im2double(rgb2gray(I)) ;
 
-%resize image
-I = imresize(I, [256 256]) ;
+%resize
+I = imresize(I, [256 256])
 
 %image thresholding
 % from R2016 version BW = imbinarize(I) ; 
 level = graythresh(I) ;
 BW = im2bw(I, level) ;
-
-
+BW = bwareafilt(BW, 1) ;
+J = I .*BW ;
+imtool([I BW J]) ;
 
