c43e69aa393b6126e17bda383c1c0f69e8803f5a
Khursheed Ali <khursheedali38@gmail.com>
2017-10-31 21:48:07 +0530

    improved psnr - replace only centre pixel

--- denoise.m
+++ denoise.m
@@ -1,18 +1,18 @@
 %reading input image
-I = imread('brain_skull.jpg');
+I = imread('noisy.png');
 
 %rgb to grayscale
 I = rgb2gray(I); 
 I = im2double(I);
 
 % %adding uniformly distributed random noise to image 
- I = imnoise(I, 'salt & pepper', 0.01) ;
+%  I = imnoise(I, 'salt & pepper', 0.01) ;
  I = imresize(I, [230 217]) ;
 J = I
 
 %selecting subimages of size 5x5 and calcualting R1, R2, R3 segments
-for i = 1:3:226
-    for j= 1:3:213
+for i = 1:1:226
+    for j= 1:1:213
         B = I( (i : i + 4), (j : j + 4) )
         sum = 0 ;
         for k = 1:5
@@ -30,7 +30,7 @@ for i = 1:3:226
 
         if sum < 5
             %noisy sub-image
-            J( (i : i + 4), (j : j + 4) ) = mean(B(:)) ;
+            J( i + 2, j + 2 ) = mean(B(:)) ;
             imshow(J) ;
         elseif sum == 5
             %edge sub-image
@@ -40,14 +40,15 @@ for i = 1:3:226
             min5index = sortIndex(1:5) ;
             [I, J] = ind2sub([5 5], min5index) ;
             meanVal = (B(I(1), J(1)) + B(I(2), J(2)) + B(I(3), J(3)) + B(I(4), J(4)) + B(I(5), J(5)) ) ./ 5 ;
-            J( (i : i + 4), (j : j + 4) ) = meanVal ;
+            J( i+2, j + 2 ) = meanVal ;
             imshow(J) 
-        else J( (i : i + 4), (j : j + 4) ) = B(1:5, 1:5) ;
+        else J( i + 2, j  + 2 ) = B(3, 3) ;
         end
     end
 end
 % 
-
+% myfilter = @filters ;		
+% J = blockproc(I, [1 1], myfilter, 'BorderSize', [2 2], 'TrimBorder', false);
 I1 = conv2(I, [1 2 1; 2 4 2; 1 2 1] ./16, 'same') ;
 I2 = medfilt2(I) ;
 
