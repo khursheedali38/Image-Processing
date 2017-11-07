9f326b0a55e0093c28cb6648b5c814d6a9d05037
Khursheed Ali <khursheedali38@gmail.com>
2017-11-07 08:00:26 +0530

    added color filtering

--- common_operations.m
+++ common_operations.m
@@ -42,13 +42,53 @@ I = imread('fill_circles_count.gif') ;
 I = imclose(I, strel('disk', 17)) ;
 imtool(I, []) ;
 
+%skull stripping
+I = im2double(imread('brain_skull.jpg')) ;
+I = rgb2gray(I) ;
+thresh = graythresh(I) ;
+BW = im2bw(I, thresh) ;
+BW = imopen(BW, strel('disk', 3)) ;
+BW = bwareafilt(BW, 1) ;
+BW = imclose(BW, strel('disk', 13)) ;
+J = I .*BW ;
+imtool([I BW J]) ;
 
+%image enhancement/contrast enhancement to color images on RGB
+I = imread('onion.png') ;
+r = I(:, :, 1) ;
+g = I(:, :, 2) ;
+b = I(:, :, 3) ;
+r1 = histeq(r) ;
+g1 = histeq(g) ;
+b1 = histeq(b) ;
+J = cat(3, r1, g1, b1) ;
+imtool([I J]) ;
 
+%image enhancement/contrast enhancement to color images on HSV
+I = imread('onion.png') ;
+I1 = rgb2hsv(I) ;
+v = histeq(I1(:, :, 3)) ;
+I2 = cat(3, I1(:, :, 1), I1(:, :, 2), v) ;
+I2 = hsv2rgb(I2) ;
+imshowpair(I2, J, 'montage') ;
 
+%color filtering - extract red color
+I = im2double(imread('coloredChips.png')) ;
+r = I(:, :, 1) ;
+g = I(:, :, 2) ;
+b = I(:, :, 3) ;
+red = graythresh(r) ;
+green = graythresh(g) ;
+blue = graythresh(b) ;
+for i = 1:size(I, 1)
+    for j = 1:size(I, 2)
+        if I(i, j, 1) < red || I(i, j, 2) > green || I(i, j, 3) > blue
+            gsc = 0.3*I(i, j, 1) + 0.59*I(i, j, 2) + 0.11*I(i, j, 3) ;
+            I(i, j, :) = [gsc gsc gsc] ;
+        end
+    end
+end
 
-
-
-
-
+imtool(I, []) ;
 
 
